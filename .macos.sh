e37cb8bcf0774a490ff2e9deac97fe6eba9ed31a
Diogo Cardoso <diogo.cardoso@flutteruki.com>
2026-04-28 14:59:22 +0100

    fix corepack enable with pnpm

--- .macos.sh
+++ .macos.sh
@@ -101,7 +101,6 @@ code --install-extension yoavbls.pretty-ts-errors
 
 echo "installing asdf plugins"
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin add pnpm https://github.com/jonathanmorley/asdf-pnpm
 asdf plugin add python https://github.com/asdf-community/asdf-python
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby
 
